5f26701fbc4249cf94abfb083efc133bfab28ad1
ved-thakre <vedantthakre7@gmail.com>
2023-07-29 22:51:02 +0530

    29-July

---
+++ 04_225A_A_dice_tower.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+		 
+  int n,x, a1 = 0, b1=0;
+  cin >> n;
+  cin >> x;
+  vector<int>s;
+  bool flag = true, ans = true;
+  for(int i=0;i<n;i++){
+	int a, b;
+	cin >> a >> b;
+	if(flag){
+	    a1 = a;
+		b1 = b;
+		flag = false;
+	}
+	if(a != a1 && a != 7-a1 && a!= b1 && a != 7-b1) ans = false;
+	if(b != a1 && b != 7-a1 && b != b1 && b != 7-b1) ans = false;
+  }
+  if(ans) cout << "YES";
+  else cout << "NO";
+	  
+  return 0;
+} 
