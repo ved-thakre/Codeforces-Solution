0dbbdbafac5b6a008690e93992c2df37cb6411c7
ved-thakre <vedantthakre7@gmail.com>
2023-08-03 22:30:00 +0530

    2-aug

---
+++ 12_1790A_Poycarp_and_day_of_pi.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+         
+int main(){
+         
+  int t;
+  cin >> t;
+  vector<int>ans;
+  string pi = "314159265358979323846264338327";
+  while(t--){
+    string s;
+    cin >> s;
+    int i;
+    for(i=0;i<s.size();i++){
+      if(s[i] == pi[i]) continue;
+      else break;
+    }
+    ans.push_back(i);
+  }
+  for(auto i: ans) cout << i << endl;    
+  
+  return 0;
+} 
+
+// Problem link - https://codeforces.com/contest/1790/problem/A
