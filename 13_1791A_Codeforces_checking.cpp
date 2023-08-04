ef4ae0d5c37f06c9e19ab5158796403194ee7b68
ved-thakre <vedantthakre7@gmail.com>
2023-08-04 20:32:58 +0530

    4-aug

---
+++ 13_1791A_Codeforces_checking.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+         
+int main(){
+         
+  int t;
+  cin >> t;
+  vector<string>ans;
+  while(t--){
+    char s;
+    cin >> s;
+    if(s != 'c' && s != 'o' && s != 'd'  && s != 'e' && s != 'f' && s != 'o' && s != 'r' && s != 's') ans.push_back("NO");
+    else ans.push_back("YES");
+    
+  }
+  for(auto i: ans) cout << i << endl;    
+  
+  return 0;
+} 
+
+// Problem link - https://codeforces.com/contest/1791/problem/A
