3ed03227eeb120a4c4d9963277c29e6653fc8ab9
ved-thakre <vedantthakre7@gmail.com>
2023-08-07 22:50:32 +0530

    7-aug

---
+++ 16_1426A_Floor_number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin >> t;
+  vector<int>ans;
+  
+  while(t--){
+      int n, x, cnt = 1;
+      cin >> n >> x;
+      n -= 2;
+      while(n > 0){
+         n -= x;
+         cnt++;
+      }
+      ans.push_back(cnt);
+  }
+  for(auto i: ans) cout << i << endl;
+      
+  return 0;
+} 
+
