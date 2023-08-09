56f6880e09fa5a2c82e6cbd99aace8dc706b4742
ved-thakre <vedantthakre7@gmail.com>
2023-08-09 19:38:44 +0530

    9-aug

---
+++ 20_580A_Kefa_and_first_steps.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+         
+int main(){
+         
+  int n, cnt = 1, maxi = 1;
+  cin >> n;
+  int arr[n];
+  for(int i=0;i<n;i++) cin >> arr[i];
+  for(int i=0;i<n-1;i++){
+    if(arr[i] <= arr[i+1]) cnt++;
+    else cnt = 1;
+    maxi = max(maxi, cnt);
+  }
+  cout << maxi;    
+  return 0;
+}
+
+// Problem link - https://codeforces.com/contest/580/problem/A
