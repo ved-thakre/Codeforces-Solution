0f4e9059f3edfe70db8b0ca6cb7459fe534839ca
ved-thakre <vedantthakre7@gmail.com>
2023-08-09 19:37:03 +0530

    9-aug

---
+++ 19_1837B_Comparison_string.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin >> t;
+  vector<int>temp;
+  while(t--){
+      int n;
+      cin >> n;
+      string s;
+      cin >> s;
+      int cnt = 1, maxi = 1;
+      for (int i = 0; i < n - 1; i++) {
+         if(s[i] == s[i + 1]) cnt ++;
+         else cnt = 1;
+         maxi = max(maxi, cnt);
+      }
+      temp.push_back(maxi+1);
+  }
+  for(auto i: temp) cout << i << endl;
+      
+  return 0;
+} 
