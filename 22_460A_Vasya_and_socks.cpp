1f8532cf297e0d88938f8dc381e371de442d6df8
ved-thakre <vedantthakre7@gmail.com>
2023-08-11 22:50:37 +0530

    11-aug

--- 22_460A_Vasya_and_socks.cpp
+++ 22_460A_Vasya_and_socks.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+         
+int main(){
+         
+  int n, m, cnt = 1;
+  cin >> n >> m;
+  while(true){
+    if(cnt%m==0)n++;
+    if(cnt >= n) break;
+    cnt++;
+  }
+  cout << cnt;
+  
+  return 0;
+} 
 
+// Problem link - https://codeforces.com/problemset/problem/460/A
