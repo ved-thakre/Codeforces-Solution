5c25141aa4e4db18dee46c3c6d0c939e6e0d2f1d
ved-thakre <vedantthakre7@gmail.com>
2023-08-25 21:55:08 +0530

    25-aug

---
+++ 43__.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+         
+int main(){
+         
+  int n;
+  cin >> n;
+  if(n%4==0 || n%7==0 || n%47==0 || n%4==74 || n%44==0 || n%77==0 || n%444==0 || n%447==0 || n%474==0 || n%477==0 || n%744==0 || n%747==0 || n%777==0) cout << "YES";
+  else{
+    string s = to_string(n);
+    for(int i=0;i<s.size();i++){
+        if(s[i] != '4' && s[i] != '7'){
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+  }
+      
+  return 0;
+} 
+
