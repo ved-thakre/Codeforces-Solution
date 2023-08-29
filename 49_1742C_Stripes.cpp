5f4a2242e940725e19f61b28b2f91a6aba859f24
ved-thakre <vedantthakre7@gmail.com>
2023-08-29 22:20:57 +0530

    29-aug

--- 49_1742C_Stripes.cpp
+++ 49_1742C_Stripes.cpp
@@ -1,57 +1,34 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool allSamecol(vector<vector<char>>& v, int col, char &ans) {
-    if (v[0][col] == 'R' || v[0][col] == 'B') {
-        char ch = v[0][col];
-        for (int i = 0; i < 8; i++) {
-            if (ch != v[i][col]) return false;
-        }
-        ans = ch;
-        return true; // Return true only if the condition is met
-    }
-    return false; // Return false otherwise
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	char g[8][8];
+	vector<int> r;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cin >> g[i][j];
+			if (g[i][j] == 'R') {r.push_back(i);}
+		}
+	}
+	for (int i : r) {
+	    bool ok = true;
+	    for (int j = 0; j < 8; j++) {
+	        if (g[i][j] != 'R') {ok = false; break;}
+	    }
+	    if (ok) {cout << "R\n"; return;}
+	}
+	cout << "B\n";
 }
 
-bool allSamerow(vector<vector<char>>& v, int row, char &ans) {
-    if (v[row][0] == 'R' || v[row][0] == 'B') {
-        char ch = v[row][0];
-        for (int i = 0; i < 8; i++) {
-            if (ch != v[row][i]) return false;
-        }
-        ans = ch;
-        return true; // Return true only if the condition is met
-    }
-    return false; // Return false otherwise
-}
-         
 int main() {
-    int t;
-    cin >> t;
-    vector<char> res;
-    while (t--) {
-        char ans;
-        vector<vector<char>> v;
-        for (int i = 0; i < 8; i++) {
-            vector<char> r(8);
-            for (int j = 0; j < 8; j++) {
-                cin >> r[j];
-            }
-            v.push_back(r);
-        }
-        for (int i = 0; i < 8; i++) {
-           if (allSamerow(v, i, ans)) {
-                res.push_back(ans);
-                break;
-           } else if (allSamecol(v, i, ans)) {
-                res.push_back(ans);
-                break;
-           }
-        }
-    }
-    for (auto i : res) {
-        cout << i << endl; 
-    }
-      
-    return 0;
-} 
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+	// solve();
+}
+
+// Problem link - https://codeforces.com/contest/1742/problem/C
