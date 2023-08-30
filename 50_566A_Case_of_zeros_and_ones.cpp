7efbdc7c84a3ec482f24b5c1adc76829ffbf9d1b
ved-thakre <vedantthakre7@gmail.com>
2023-08-30 23:02:44 +0530

    30-aug

--- 50_566A_Case_of_zeros_and_ones.cpp
+++ 50_566A_Case_of_zeros_and_ones.cpp
@@ -1,30 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-         
-int main(){
 
-  int n;
-  cin >> n;
-  vector<int>arr(n);
-  for(int i=0;i<n-1;i++) cin >> arr[i];
-  while(true){
-    vector<int>temp;
-    bool doesExist = false;
-    for(int i=0;i<n-1;i++){
-        if((arr[i] == 0 && arr[i+1] == 1) || (arr[i] == 1 && arr[i+1] == 0)){
-            arr[i] = -1;
-            arr[i+1] = -1;
-            doesExist = true;
-        }
+int main() {
+    int n;
+    cin >> n;
+    vector<char> arr(n);
+    int zcnt = 0 , ocnt = 0;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i] == '0')zcnt++;
+        else ocnt++;
     }
-    if(!doesExist) break;
-    for(int i=0;i<n-1;i++){
-        if(arr[i] != -1) temp.push_back(arr[i]);
-    }
-    arr = temp;
-    temp.clear();
-  }
-  cout << arr.size();
-      
-  return 0;
-} 
+    cout << n - (2 * min(zcnt,ocnt));
+    
+
+    return 0;
+}
