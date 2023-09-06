94183cdd08d8ce1cdb1d4dfa5b7d844522c2f3d8
ved-thakre <vedantthakre7@gmail.com>
2023-09-06 22:09:00 +0530

    6-sept

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int n, sum = 0;
+        cin >> n;
+        int arr[1001];
+        for (int i = 0; i < n; i++)
+        {
+                cin >> arr[i];
+                sum += arr[i];
+        }
+
+        if ((sum == n - 1 && n != 1) || (n == 1 && arr[0] == 1))
+                cout << "YES";
+        else
+                cout << "NO";
+
+        return 0;
+}
