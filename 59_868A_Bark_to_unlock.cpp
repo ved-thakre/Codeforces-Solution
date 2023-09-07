19d29fc503a399e59de0c982a7a2f5d77397b5cc
ved-thakre <vedantthakre7@gmail.com>
2023-09-07 21:57:33 +0530

    7-sept

--- 59_868A_Bark_to_unlock.cpp
+++ 59_868A_Bark_to_unlock.cpp
@@ -3,36 +3,45 @@ using namespace std;
          
 int main(){
          
-   string pass;
-   cin >> pass;
+   string s, pass;
    int n;
-   cin >> n;
-   vector<string>arr(n);
-   for(int i=0;i<n;i++) cin >> arr[i];
-
-   char first = pass[0] , second = pass[1];
-   
-   bool one = false, two = false;
-   for(int i=0;i<n;i++){
-    if(arr[i][0] == first) one = true;
-    if(arr[i][1] == second) two = true;
-   }
-   if(one && two){
-        cout << "YES";
-        return 0;
-   }else{
-        one = false;
-        two = false;
-        for(int i=0;i<n;i++){
-            if(arr[i][0] == second) one = true;
-            if(arr[i][1] == first) two = true;
+   cin >> pass >> n;
+   if(n == 1){
+        cin>>s;
+        if(pass == s){
+            cout<<"YES"<<endl;
         }
-     if(one && two) {
-            cout << "YES";
-            return 0;
-     }
-   }
-   cout << "NO";
+        else{
+            reverse(s.begin(),s.end());
+            if(pass == s){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+        return 0;
+    }
+    int flag = 0,okf = 0, okl = 0;
+    while(n--){
+        cin>>s;
+        if(pass[0] == s[1]){
+            okf = 1;
+        }
+        if(pass[1] == s[0]){
+            okl = 1;
+        }
+        if(pass == s){
+            flag = 1;
+        }
+    }
+    if(flag == 1 || (okf == okl && okf == 1)){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
       
    return 0;
 } 
