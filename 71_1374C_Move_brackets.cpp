2998b6b8b5d24d1a1b293c97b1b47947b77de830
ved-thakre <vedantthakre7@gmail.com>
2023-09-24 22:42:55 +0530

    24-sept

---
+++ 71_1374C_Move_brackets.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a;
+        cin >> a;
+
+        string n;
+        cin >> n;
+
+        int ans = a / 2;
+        stack<char> start;
+        int count = 0;
+
+        for (int i = 0; i < n.length(); ++i)
+        {
+            if (n[i] == '(')
+            {
+                start.push(n[i]);
+            }
+            else if (start.empty())
+            {
+                count++;
+            }
+            else
+            {
+                start.pop();
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
